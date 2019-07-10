1e7d5bbd46e8c439b81744676435befe4179f8d7
Markus Hirzberger <h1651872@wu.ac.at>
2019-07-10 20:16:01 +0200

    flask runs..

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
+from flask import Flask
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,23 @@
+from app import app, db, login_manager, models, bcrypt
+from flask import render_template, flash, redirect, request
+from flask_login import login_user, logout_user, login_required
+from app.login import LoginForm, RegistrationForm
+from app.models import User
 
+
+# landingpage general
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+from app import app, db, login_manager, models, bcrypt
+from flask import render_template, flash, redirect, request
+from flask_login import login_user, logout_user, login_required
+from app.login import LoginForm, RegistrationForm
+from app.models import User
+
+
+# landingpage general
+@app.route("/")
+def index():
+    return render_template('index.html')
